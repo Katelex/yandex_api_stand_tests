4b975b9cfed438b719503d52f2125f8c53f908b2
Kate <kattysv05@yandex.ru>
2023-01-14 14:58:24 +1100

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://d6c299c9-a2a6-49fd-95b6-5deba8cafab9.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
